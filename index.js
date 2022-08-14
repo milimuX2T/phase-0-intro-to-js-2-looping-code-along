31f3756cfe64a0657a5aea11dc8602f61c066794
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-14 15:17:15 +0300

    Lab Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function countDown (num){
+    let current = num;
+    while (current > -1) {
+        console.log(current);
+        current = current - 1
+    }
+
+}
+countDown (num)
+
+function writeCards(name, event) {
+    const result = [];
+    for (let i = 0; i < name.length; i++){
+    result.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return result;  
+}
